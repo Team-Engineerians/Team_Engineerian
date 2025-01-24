cf97be491b08a0b28e64396341109e9a4a99727f
AdityaCubicles45 <67513926+AdityaCubicles45@users.noreply.github.com>
2025-01-24 21:52:14 +0530

    Infinite scroll added

--- the_engineerians/tailwind.config.ts
+++ the_engineerians/tailwind.config.ts
@@ -8,6 +8,19 @@ export default {
       fontFamily: {
         sans: ["var(--font-geist-sans)", ...fontFamily.sans],
       },
+      keyframes: {
+        "infinite-scroll": {
+          from: { transform: "translateX(0)" },
+          to: { transform: "translateX(-100%)" },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+        "animate-frame-contact-me-btn-icon":
+          "frame-contact-me-btn-icon .3s ease",
+        "infinite-scroll": "infinite-scroll 25s linear infinite",
+      },
     },
   },
   plugins: [],
