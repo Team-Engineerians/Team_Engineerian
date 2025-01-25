db89d1858789b106e76e644f14d15fbf26d3eb02
Meet <meetaparekh2003@gmail.com>
2025-01-25 18:18:59 +0530

    About Component

--- the_engineerians/src/app/_components/About.tsx
+++ the_engineerians/src/app/_components/About.tsx
@@ -1,10 +1,33 @@
 import React from 'react'
+import AboutCard from './ui/AboutCard'
 
 type Props = {}
 
 const About = (props: Props) => {
   return (
-    <div>About</div>
+    <div className='h-screen w-full bg-[#fffff] flex justify-center items-center' >
+      <div className='flex flex-col text-white justify-center text-center h-[500px] w-[1157px]   gap-[63px]' >
+        <div className='flex flex-col' >
+          <h1 className=' text-[55px] ' >What we do</h1>
+          <h1 className='font-thin text-[55px] opacity-50' >inspires confidence</h1>
+        </div>
+        <div className=" flex flex-row gap-10  " >
+            <div className="flex flex-col gap-4 items-center justify-center  w-[400px] h-[300px]  rounded-xl border-2 border-opacity-10 border-white">
+                <h2 className='text-[26px] w-[118] opacity-50 ' >Innovative Designs</h2>
+                <p className='w-[207px]' >Crafting unique, user-centered designs that inspire and connect.</p>
+            </div>
+            <div className="flex flex-col gap-4 items-center justify-center  w-[400px] h-[300px] border-2 border-opacity-10 border-white rounded-xl">
+                <h2 className='text-[26px] w-[118] opacity-50' >Flawless Experiences</h2>
+                <p className='w-[207px]' >Seamlessly blending creativity and functionality to captivate users.</p>
+            </div>
+            <div className="flex flex-col gap-4 items-center justify-center  w-[400px] h-[300px] border-2 border-opacity-10 border-white rounded-xl">
+                <h2 className='text-[26px] w-[118]  opacity-50 ' >Scalable Solutions</h2>
+                <p className='w-[207px]' >Building systems designed to grow with your ambitions.</p>
+            </div>
+        </div>
+        <p className='opacity-50 ' >Redefining possibilities with solutions that inspire confidence and drive results.</p>
+      </div>
+    </div>
   )
 }
 
--- the_engineerians/src/app/page.tsx
+++ the_engineerians/src/app/page.tsx
@@ -3,6 +3,7 @@ import '../styles/globals.css';
 import { api, HydrateClient } from "~/trpc/server";
 // import InfiniteScroll from './_components/InfiniteScroll';
 import Hero from './_components/Hero';
+import About from './_components/About';
 
 export default async function Home() {
   // const hello = await api.post.hello({ text: "from tRPC" });
@@ -14,6 +15,7 @@ export default async function Home() {
       <main className="flex min-h-screen flex-col items-center justify-center bg-[#101010]">
         <Hero />
         {/* <InfiniteScroll /> */}
+        <About/>
       </main>
     </HydrateClient>
   );
